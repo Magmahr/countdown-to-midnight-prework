7a1b340010f2c8cdb6e2a4f5424b9e74ad360d84
Matt Maher <mattmaher0@gmail.com>
2018-05-14 14:09:50 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -15,14 +15,14 @@ end
 def countdown_with_sleep(n)
   aim = 0
   while n > aim
-  return "#{n} SECONDS!"
+  puts "#{n} SECONDS!"
   sleep 1
     if n == 1
-      return "1 SECOND!"
+      puts "1 SECOND!"
       sleep 1
     end  
     n -= 1
   end 
   sleep 1
-  return "HAPPY NEW YEAR!"
+ "HAPPY NEW YEAR!"
 end
