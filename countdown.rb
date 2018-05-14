fa09adcf99c878a89e521b0742b7a19c09e4c849
Matt Maher <mattmaher0@gmail.com>
2018-05-14 13:59:56 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,18 @@ def countdown(n)
   aim = 0
   while n > aim
   return "#{n} SECONDS!"
+    if n == 1
+      return "1 SECOND!"
+    end  
+    n -= 1
+  end  
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  aim = 0
+  while n > aim
+  return "#{n} SECONDS!"
   sleep 1
     if n == 1
       return "1 SECOND!"
