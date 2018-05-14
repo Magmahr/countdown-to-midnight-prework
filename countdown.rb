cba86e0348b8641fc7bbc4090a95ffece4757cff
Matt Maher <mattmaher0@gmail.com>
2018-05-14 14:01:27 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -16,9 +16,10 @@ def countdown_with_sleep(n)
   aim = 0
   while n > aim
   return "#{n} SECONDS!"
-  sleep 2
+  sleep 1
     if n == 1
       return "1 SECOND!"
+      sleep 1
     end  
     n -= 1
   end  
